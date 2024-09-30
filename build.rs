36f04f0e7c9a3dbef4737a5784d22d6bd59cf8e5
drakeerv <drakeerv@outlook.com>
2024-09-30 15:27:40 -0400

    Fix action detect

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ use chrono::Local;
 fn main() {
     let build_time = Local::now().to_rfc2822();
     println!("cargo:rustc-env=BUILD_DATE={}", build_time);
-    let used_actions = std::env::var("GITHUB_ACTIONS").unwrap_or_default() == "true";
-    println!("cargo:rustc-env=USED_ACTIONS={}", used_actions);
+    let action_name = std::env::var("GITHUB_ACTIONS").unwrap_or("Actions not detected".to_string());
+    println!("cargo:rustc-env=ACTION_NAME={}", action_name);
 }
